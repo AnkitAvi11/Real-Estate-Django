37807be8481b8746875290c7f2dbc07bbb7ba1dc
AnkitKumar1311 <kumar.ankit383@gmail.com>
2020-06-16 18:05:11 +0530

    Minor bugs fixed in home and about page

--- listing/admin.py
+++ listing/admin.py
@@ -6,7 +6,7 @@ from listing.models import Listing
 class ListingAdmin(admin.ModelAdmin) : 
     list_display = ('id', 'title', 'realtor', 'state', 'city', 'pub_date', 'zipcode')
     list_display_links = ('id', 'title')
-    list_filter = ('state', 'state', 'realtor')
+    list_filter = ('state', 'realtor')
     search_fields = ('title', 'state')
 
 admin.site.register(Listing, ListingAdmin)
--- pages/views.py
+++ pages/views.py
@@ -1,10 +1,20 @@
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render, redirect
+from realtor.models import Realtor
+from listing.models import Listing
 
 def indexPage(request) : 
-    return render(request, 'pages/index.html')
+    listings = Listing.objects.all().order_by('-pub_date')[:3]
+    context = {
+        "listings" : listings
+    }
+    return render(request, 'pages/index.html', context)
 
 def aboutPage(request) :
-    return render(request, 'pages/about.html')
+    mvp = Realtor.objects.filter(is_mvp=True)[0]
+    context = {
+        "realtor" : mvp
+    }
+    return render(request, 'pages/about.html', context)
 
 
